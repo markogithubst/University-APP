bbb9d9b8ee83b3c02c4c760cfe64fc5c51fffca8
Marko Vukusic <marko.vukusic@agilathon.com>
2023-01-17 15:53:52 +0100

    adding models and config folders

---
+++ models/professor.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Professor extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Professor.init({
+    name: DataTypes.STRING,
+    address: DataTypes.STRING,
+    phoneNumber: DataTypes.INTEGER,
+    departmentId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Professor',
+  });
+  return Professor;
+};
---
+++ models/student.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Student extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Student.init({
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    address: DataTypes.STRING,
+    phoneNumber: DataTypes.INTEGER,
+    majorId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Student',
+  });
+  return Student;
+};
