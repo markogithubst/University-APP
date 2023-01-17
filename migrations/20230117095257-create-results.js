f32f5075c4a474de05bbe46d721e9b6766eb9dd3
Marko Vukusic <marko.vukusic@agilathon.com>
2023-01-17 15:57:34 +0100

    added migrations folder

---
+++ migrations/20230117095257-create-results.js
@@ -0,0 +1,30 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Results', {
+      studentId: {
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      grade: {
+        type: Sequelize.INTEGER
+      },
+      examId: {
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Results');
+  }
+};
