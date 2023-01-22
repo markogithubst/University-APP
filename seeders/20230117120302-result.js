e28888f1798d14cbb7bf1397bcefbe8b2d58a94b
Marko Vukusic <marko.vukusic@agilathon.com>
2023-01-22 13:31:18 +0100

    modified examId fields

--- seeders/20230117120302-result.js
+++ seeders/20230117120302-result.js
@@ -16,49 +16,49 @@ module.exports = {
     {
       StudentId: "1",
       grade: "5",
-      ExamId: "7",
+      ExamId: "1",
       createdAt: new Date(),
       updatedAt: new Date()
     },
     {
       StudentId: "2",
       grade: "2",
-      ExamId: "7",
+      ExamId: "2",
       createdAt: new Date(),
       updatedAt: new Date()
     },
     {
       StudentId: "3",
       grade: "2",
-      ExamId: "8",
+      ExamId: "3",
       createdAt: new Date(),
       updatedAt: new Date()
     },
     {
       StudentId: "4",
       grade: "1",
-      ExamId: "9",
+      ExamId: "4",
       createdAt: new Date(),
       updatedAt: new Date()
     },
     {
       StudentId: "5",
       grade: "3",
-      ExamId: "10",
+      ExamId: "5",
       createdAt: new Date(),
       updatedAt: new Date()
     },
     {
       StudentId: "6",
       grade: "2",
-      ExamId: "11",
+      ExamId: "6",
       createdAt: new Date(),
       updatedAt: new Date()
     },
     {
       StudentId: "1",
       grade: "2",
-      ExamId: "12",
+      ExamId: "3",
       createdAt: new Date(),
       updatedAt: new Date()
     }
