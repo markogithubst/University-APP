48825b5234a7fe4d0597f0ecb3320dabdc24e951
Marko Vukusic <marko.vukusic@agilathon.com>
2023-01-24 10:13:36 +0100

    corrected tableName in model

--- models/enrollment.js
+++ models/enrollment.js
@@ -24,7 +24,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'Enrollment',
-    department: 'enrollment'
+    tableName: 'enrollment'
   });
   Enrollment.removeAttribute('id');
   return Enrollment;
