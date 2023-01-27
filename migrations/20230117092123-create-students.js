c84ce68c277def80ed6f28d956a7eee59102e123
Marko Vukusic <marko.vukusic@agilathon.com>
2023-01-27 14:32:46 +0100

    modified name to be full_name

--- migrations/20230117092123-create-students.js
+++ migrations/20230117092123-create-students.js
@@ -9,7 +9,7 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      name: {
+      full_name: {
         allowNull: false,
         type: Sequelize.STRING
       },
