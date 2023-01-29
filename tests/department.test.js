46e58a8aef8f05684002f27bf7b783a254d3e404
Marko Vukusic <marko.vukusic@agilathon.com>
2023-01-29 21:43:04 +0100

    created few GET department tests

--- tests/department.test.js
+++ tests/department.test.js
@@ -1,10 +1,24 @@
 /* eslint-disable no-undef */
 const request = require('supertest');
 const app = require('../app');
+const { sequelize } = require('../models/index');
 
-describe('GET /departments', () => {
-  it('should return a list of departments', async () => {
-    const result = await request(app).get('/departments');
-    expect(result.statusCode).toEqual(200);
+describe('Testing GET department route', () => {
+  test('should respond with a 200 status code to GET all departments', async () => {
+    const response = await request(app).get('/departments');
+    expect(response.statusCode).toBe(200);
+  });
+  test('should respond with a 200 status code to GET one department', async () => {
+    const departmentId = 3;
+    const response = await request(app).get(`/departments/${departmentId}`);
+    expect(response.statusCode).toBe(200);
+  });
+  test('should respond with a 404 status code for not finding department', async () => {
+    const departmentId = 50;
+    const response = await request(app).get(`/departments/${departmentId}`);
+    expect(response.statusCode).toBe(404);
+  });
+  afterAll(async () => {
+    await sequelize.close();
   });
 });
