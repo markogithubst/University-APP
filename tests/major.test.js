ef8f849b5159280a7a3cd6a8fd7a25e0af04622f
Marko Vukusic <marko.vukusic@agilathon.com>
2023-01-30 14:08:53 +0100

    added migrate/seed/undo scripts to test files

--- tests/major.test.js
+++ tests/major.test.js
@@ -3,23 +3,23 @@ const request = require('supertest');
 const app = require('../app');
 const { execSync } = require('child_process');
 
-describe('Testing GET DEPARTMENT route', () => {
+describe('Testing GET MAJOR route', () => {
   beforeAll(() => {
     execSync('npm run migrate:test');
     execSync('npm run seed:test');
   });
-  test('should respond with a 200 status code to GET all departments', async () => {
-    const response = await request(app).get('/departments');
+  test('should respond with a 200 status code to GET all MAJORS', async () => {
+    const response = await request(app).get('/majors');
     expect(response.statusCode).toBe(200);
   });
   test('should respond with a 200 status code to GET one department', async () => {
-    const departmentId = 3;
-    const response = await request(app).get(`/departments/${departmentId}`);
+    const majorId = 1;
+    const response = await request(app).get(`/majors/${majorId}`);
     expect(response.statusCode).toBe(200);
   });
   test('should respond with a 404 status code for not finding department', async () => {
-    const departmentId = 50;
-    const response = await request(app).get(`/departments/${departmentId}`);
+    const majorId = 88;
+    const response = await request(app).get(`/majors/${majorId}`);
     expect(response.statusCode).toBe(404);
   });
   afterAll(() => {
