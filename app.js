6209775a784df1bd957992e20d54ed9133d48e51
Marko Vukusic <marko.vukusic@agilathon.com>
2023-01-30 13:33:07 +0100

    moved app.listen to server.js

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const router = require('./routes/index');
+
+app.use(express.json());
+app.use('/', router);
+
+app.get('/', (req, res) => {
+  res.send('<h1>Connection test</h1>');
+});
+
+module.exports = app;
