28729b699c460d45d246e1f4dba5932f64f60792
Marko Vukusic <marko.vukusic@agilathon.com>
2023-02-01 16:25:03 +0100

    added collectCoverage to generate code coverage report

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,8 @@ const config = {
   reporters: [
     'default',
     ['./node_modules/jest-html-reporter', { pageTittle: 'Test Report' }]
-  ]
+  ],
+  collectCoverage: true
 };
 
 module.exports = config;
