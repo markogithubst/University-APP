ed1a478df8a924e6ea25ee5b18e0819a7ca8f077
Marko Vukusic <marko.vukusic@agilathon.com>
2023-02-03 15:44:08 +0100

    added checking of response.body toBeInstanceOf

--- tests/professor.test.js
+++ tests/professor.test.js
@@ -22,6 +22,7 @@ describe('Testing all PROFESSOR routes', () => {
       const response = await request(app).get('/professors');
       expect(response.statusCode).toBe(200);
       expect(response.headers['content-type']).toMatch(/json/);
+      expect(response.body).toBeInstanceOf(Array);
     });
     describe.each([
       [3, 200],
