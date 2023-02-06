b1b809c063f626e91509f45c848de7c841115e88
Marko Vukusic <marko.vukusic@agilathon.com>
2023-02-06 14:27:49 +0100

    added swagger doc

--- routes/enrollmentRouter.js
+++ routes/enrollmentRouter.js
@@ -4,11 +4,37 @@ const router = express.Router();
 const enrollmentController = require('../controllers/enrollmentController');
 const { validateId, validateDoubleId, validateEnrollment } = require('../middleware');
 
-router.get('/student/:id', validateId, enrollmentController.getEnrollmentsByStudent);
-router.get('/course/:id', validateId, enrollmentController.getEnrollmentsByCourse);
-router.get('/', enrollmentController.getAllEnrollments);
-router.post('/', validateEnrollment, enrollmentController.createEnrollment);
-router.put('/:firstId/:secondId', validateDoubleId, validateEnrollment, enrollmentController.updateEnrollment);
-router.delete('/:firstId/:secondId', validateDoubleId, enrollmentController.deleteEnrollment);
+router.get('/student/:id',
+/* #swagger.tags = ['Enrollment'] */
+  validateId, enrollmentController.getEnrollmentsByStudent);
+
+router.get('/course/:id',
+/* #swagger.tags = ['Enrollment'] */
+  validateId, enrollmentController.getEnrollmentsByCourse);
+
+router.get('/',
+/* #swagger.tags = ['Enrollment'] */
+  /* #swagger.responses[200] = {
+        description: 'Get all Enrollments from the database',
+        schema: [
+            {
+                "course_id": 1,
+                "student_id": 3
+            }
+        ]
+    } */
+  enrollmentController.getAllEnrollments);
+
+router.post('/',
+/* #swagger.tags = ['Enrollment'] */
+  validateEnrollment, enrollmentController.createEnrollment);
+
+router.put('/:firstId/:secondId',
+/* #swagger.tags = ['Enrollment'] */
+  validateDoubleId, validateEnrollment, enrollmentController.updateEnrollment);
+
+router.delete('/:firstId/:secondId',
+/* #swagger.tags = ['Enrollment'] */
+  validateDoubleId, enrollmentController.deleteEnrollment);
 
 module.exports = router;
--- routes/majorRouter.js
+++ routes/majorRouter.js
@@ -4,10 +4,32 @@ const router = express.Router();
 const majorController = require('../controllers/majorController');
 const { validateName, validateId } = require('../middleware');
 
-router.get('/:id', validateId, majorController.getOneMajor);
-router.get('/', majorController.getAllMajors);
-router.post('/', validateName, majorController.createMajor);
-router.put('/:id', validateId, validateName, majorController.updateMajor);
-router.delete('/:id', validateId, majorController.deleteMajor);
+router.get('/:id',
+/* #swagger.tags = ['Major'] */
+  validateId, majorController.getOneMajor);
+
+router.get('/',
+/* #swagger.tags = ['Major'] */
+  /* #swagger.responses[200] = {
+        description: 'Get all Majors from the database',
+        schema: [
+              {
+                  "name": "Electrical Engineering and Information Technology"
+              }
+          ]
+    } */
+  majorController.getAllMajors);
+
+router.post('/',
+/* #swagger.tags = ['Major'] */
+  validateName, majorController.createMajor);
+
+router.put('/:id',
+/* #swagger.tags = ['Major'] */
+  validateId, validateName, majorController.updateMajor);
+
+router.delete('/:id',
+/* #swagger.tags = ['Major'] */
+  validateId, majorController.deleteMajor);
 
 module.exports = router;
