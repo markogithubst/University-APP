3e4debf0c33b747624b42d668a7bee2bff1c02e7
Marko Vukusic <marko.vukusic@agilathon.com>
2023-02-06 16:37:45 +0100

    imported department error msg schemas from swagger.js

--- swagger.js
+++ swagger.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-useless-escape */
 // eslint-disable-next-line import/no-extraneous-dependencies
 const dotenv = require('dotenv');
 dotenv.config();
@@ -13,7 +14,47 @@ const doc = {
     description: 'Simple API overview'
   },
   host: `localhost:${process.env.PORT}`,
-  schemes: ['http']
+  schemes: ['http'],
+  components: {
+    schemas: {
+      DepartmentResponseSuccessful: {
+        type: 'object',
+        properties: {
+          name: {
+            type: 'string',
+            example: 'Department of Mathematics and Physics'
+          }
+        }
+      },
+      NoDepartmentError: {
+        type: 'object',
+        properties: {
+          message: {
+            type: 'string',
+            example: 'Item not found'
+          }
+        }
+      },
+      NoConnectionDbError: {
+        type: 'object',
+        properties: {
+          error: {
+            type: 'string',
+            example: 'connect ECONNREFUSED localhost:DB_PORT'
+          }
+        }
+      },
+      ValidationJoiError: {
+        type: 'object',
+        properties: {
+          message: {
+            type: 'array',
+            example: ['\"id\" must be greater than or equal to 1', '\"id\" must be a number']
+          }
+        }
+      }
+    }
+  }
 };
 
 const outputFile = './swagger-output.json';
